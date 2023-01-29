7691a2eaa3b4594133791a5a08de592f7c7af800
Md Asmaul Hossain Sajib <ssajib902@gmail.com>
2023-01-29 12:01:45 +0600

    second commit

--- loop.js
+++ loop.js
@@ -1,3 +1,23 @@
 var bottleItem = 'water';
 var bottleColor = 'yellow';
 var bottlePrice = 50;
+var penColor = 'blue';
+
+var isExpensive = true;
+
+var items = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+
+var item3 = items[3];
+items[4] = 'notebook';
+var penIndex = items.indexOf('pen');
+
+if (items.length > 4) {
+
+}
+else if (items[3] == 'pen') {
+
+}
+
+else {
+    console.log()
+}
